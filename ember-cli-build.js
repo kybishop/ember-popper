75d4cccbeca1a3e3bad3d190d02c98e309b9553e
Kyle Bishop <kybishop@gmail.com>
2017-07-08 10:56:41 -0700

    chore[bump deps] popper.js v1.10.8 and other dep bumps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,15 @@
 /* eslint-env node */
+'use strict';
+
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
+  let app = new EmberAddon(defaults, {
     // Add options here
+    fingerprint: {
+      enabled: true,
+      generateAssetMap: true
+    }
   });
 
   /*
