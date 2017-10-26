f1f84dbcf4640d5321b255c58ebabc0e253688b1
Christopher Garrett <me@pzuraq.com>
2017-10-26 09:31:04 -0700

    refactor(decorators): Refactor to use ember-argument-decorators

--- addon/components/ember-popper.js
+++ addon/components/ember-popper.js
@@ -1,14 +1,12 @@
-import Ember from 'ember';
+import { guidFor } from '@ember/object/internals';
 import EmberPopperBase from './ember-popper-base';
-
-const { generateGuid } = Ember;
 
 export default class EmberPopper extends EmberPopperBase {
 
   // ================== LIFECYCLE HOOKS ==================
 
   init() {
-    this.id = this.id || generateGuid();
+    this.id = this.id || `${guidFor(this)}-popper`;
     this._parentFinder = self.document ? self.document.createTextNode('') : '';
 
     super.init(...arguments);
