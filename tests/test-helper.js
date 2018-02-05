ce0925b869c0d5a65c8c0422b6fe30e4889e2448
Christopher Garrett <me@pzuraq.com>
2018-02-05 12:10:48 -0800

    add test waiter

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,9 @@
 import Application from '../app';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import registerRAFWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 
+registerRAFWaiter();
 setApplication(Application.create({ autoboot: false }));
 
 start();
