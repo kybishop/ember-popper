b488ee99017d95a6b5cf904da4ebedc638898bc9
Kyle Bishop <kybishop@gmail.com>
2018-10-12 16:16:10 -0700

    fixups

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,69 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-1.13',
-          bower: {
-            dependencies: {
-              'ember': '~1.13.0',
-              'ember-cli-shims': '0.0.6'
-            },
-            resolutions: {
-              'ember': '~1.13.0',
-              'ember-cli-shims': '0.0.6'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-cli-shims': null,
-              'ember-native-dom-event-dispatcher': null,
-              'ember-source': null
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.4',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-4'
-            },
-            resolutions: {
-              'ember': 'lts-2-4'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-native-dom-event-dispatcher': null,
-              'ember-source': null
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-8'
-            },
-            resolutions: {
-              'ember': 'lts-2-8'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-native-dom-event-dispatcher': null,
-              'ember-source': null
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-native-dom-event-dispatcher': '^0.6.3',
-              'ember-source': '~2.12.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-2.16',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
