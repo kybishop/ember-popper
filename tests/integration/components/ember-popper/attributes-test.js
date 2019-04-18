1f58dcee7b773fa3f1d6e9136c989159c036ddc9
simonihmig <simon.ihmig@kaliber5.de>
2019-04-19 01:35:44 +0200

    Support angle bracket invocation
    Propagates attributes using "splattributes".
    
    Closes #98

--- tests/integration/components/ember-popper/attributes-test.js
+++ tests/integration/components/ember-popper/attributes-test.js
@@ -54,4 +54,18 @@ module('Integration | Component | attributes', function(hooks) {
 
     assert.dom('#foo').hasAttribute('role', 'tooltip', 'role attribute bound correctly');
   });
+
+  test('any attributes are supported with angle bracket invocation', async function(assert) {
+    await render(hbs`
+      <div class='parent' style='position: fixed; bottom: 0; height: 100px; width: 100%;'>
+        <EmberPopperTargetingParent @id='foo' @placement='top' role='tooltip' title="bar" data-test-foo>
+          test
+        </EmberPopperTargetingParent>
+      </div>
+    `);
+
+    assert.dom('#foo').hasAttribute('role', 'tooltip', 'role attribute bound correctly');
+    assert.dom('#foo').hasAttribute('title', 'bar', 'title attribute bound correctly');
+    assert.dom('#foo').hasAttribute('data-test-foo');
+  });
 });
